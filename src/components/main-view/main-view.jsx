de2f3f9456b4920a3e5b588d760a24cd59f88d4a
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-05-23 13:29:11 -0700

    Loading data from API
    imported external API to main-view, updated movie-card file with prop-types and incorporated prop-types into the package.json file.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,47 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Seven",
-      image:
-        "seven.jpg",
-      director: "David Fincher",
-    },
-    {
-      id: 2,
-      title: "The Godfather",
-      image:
-        "thegodfather.jpg",
-      director: "Francis Ford Coppola",
-    },
-    {
-      id: 3,
-      title: "The Shawshank Redemption",
-      image:
-        "shawshankredemption.jpg",
-      director: "Frank Darabont",
-    },
-    {
-      id: 4,
-      title: "Inception",
-      image:
-        "inception.jpg",
-      director: "Christopher Nolan",
-    },
-    {
-      id: 5,
-      title: "Silence of the Lambs",
-      image:
-        "silenceofthelambs.jpg",
-      director: "Jonathan Demme",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://flix-vault-253ef352783e.herokuapp.com/")
+    .then((response) => response.json())
+    .then((data) => {
+      const moviesFromApi = data.movies.map((movies) => {
+        return {
+          id: movies.id,
+          title: movies.title,
+          image: movies.ImagePath,
+          director: movies.director_name?.[0],
+        };
+      });
+
+      setMovies(moviesFromApi);
+    });
+  }, []);
 
   if (selectedMovie) {
     return (
