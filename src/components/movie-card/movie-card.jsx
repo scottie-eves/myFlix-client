f3cd6f9b5234a1e8a5a151b13edb8be043d628f9
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-06-05 16:11:46 -0700

    Bootstrap UI design incorporations
    Installed React Bootstrap. Imported bootstrap correctly to src-files. Applied responsive grid views using bootstrap with containers, rows and columns. Incorporated forms, cards and buttons using bootstrap.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,21 @@
+<<<<<<< Updated upstream
+=======
+import PropTypes from "prop-types";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+>>>>>>> Stashed changes
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
+      <Card className="h-100" onClickCapture={() => onMovieClick(movie)}>
+        <Card.Img variant="top" src={movie.image} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.director}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">
+            Open
+          </Button>
+        </Card.Body>
+      </Card>
     );
   };
