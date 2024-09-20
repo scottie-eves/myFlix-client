e4f756d2b64bcce9bf2e80cc12d5a15d6f4c26c0
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-09-20 12:20:33 -0700

    updated moviecard and view
    added Row, Col for the Card implementation of the movies to be shown. The last error I was getting after fixing the mainview problem was the movie card not reading correctly.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,18 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movies }) => {
+export const MovieCard = ({ movies, addFavorite, deleteFavorite, user }) => {
+
+  const isFavorite = user.FavoriteMovies.includes(movies._id);
+  
     return (
       <Card className="h-100">
         <Card.Img variant="top" src={movies.image} />
         <Card.Body>
           <Card.Title>{movies.Title}</Card.Title>
           <Card.Text>{movies.Director}</Card.Text>
-          <Link to={`/movies/${encodeURIComponent(movies._id)}`}>
-            <Button variant="link">Open</Button>
-          </Link>
+          <Row>
+          <Col>
+            <Link to={`/movies/${encodeURIComponent(movies._id)}`}>
+              <Button variant="primary" className="primary-button_custom">
+                Open
+              </Button>
+            </Link>
+          </Col>
+          <Col>
+            {isFavorite ? (
+              <Button
+                variant='primary'
+                className='primary-button_custom'
+                onClick={() => deleteFavorite(movies._id)}
+              >
+                Unfavorite
+              </Button>
+            ) : (
+              <Button
+                variant='primary'
+                className='primary-button_custom'
+                onClick={() => addFavorite(movies._id)}
+              >
+                Favorite
+              </Button>
+            )}
+          </Col>
+        </Row>
         </Card.Body>
       </Card>
     );
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,28 +1,37 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
   const { movieId } = useParams();
   const movie = movies.find((movie) => movie._id === movieId);
 
+  if (!movie) {
+    console.error(`No movie found with ID: ${movieId}`);
+    return <div>No movie was found.</div>;
+  }
+
     return (
-      <div>
-        <div>
-          <img src={movie.ImagePath} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.Title}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.Director}</span>
-        </div>
-        <Link to={`/`}>
-        <Button variant="info">Back</Button>
-      </Link>
-      </div>
+      <Card className="h-100">
+        <Row>
+          <Col>
+          <Card.Img variant="top" src={movies.image} />
+          </Col>
+          <Col>
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>{"Director: " + movie.Director}</Card.Text>
+            <Card.Text>{"Genre: " + movie.Genre}</Card.Text>
+            <Card.Text>{"Description: " + movie.Description}</Card.Text>
+            <Link to="/">
+            <Button variant="info" className="primary-button_custom">Back</Button>
+            </Link>
+          </Card.Body>
+          </Col>
+        </Row>
+      </Card>
     );
   };
